8bf84056450236c6222c17c3876c88722e0641a5
Ulises <ulisesdejesusk9999@gmail.com>
2026-08-15 12:34:41 -0600

    feat(books): enforce unique book titles
    - Add unique constraint for book titles
    - Improve error handling for database conflicts

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,39 +1,82 @@
 import { PrismaService } from '@/prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class BooksService {
   constructor(private readonly prisma: PrismaService) {}
+
   findAll() {
     return this.prisma.book.findMany();
   }
 
-  create(dto: CreateBookDto) {
-    return this.prisma.book.create({
-      data: dto,
-    });
+  async create(dto: CreateBookDto) {
+    try {
+      const book = await this.prisma.book.create({
+        data: dto,
+      });
+
+      return book;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new BadRequestException('Book title already exists');
+        }
+      }
+      throw error;
+    }
   }
 
-  findOne(id: string) {
-    return this.prisma.book.findUnique({
+  async findOne(id: string) {
+    const book = await this.prisma.book.findUnique({
       where: {
         id,
       },
     });
+
+    if (!book) {
+      throw new NotFoundException('Book not found');
+    }
+
+    return book;
   }
 
-  update(id: string, dto: UpdateBookDto) {
-    return this.prisma.book.update({
-      where: { id },
-      data: dto,
-    });
+  async update(id: string, dto: UpdateBookDto) {
+    try {
+      const book = await this.prisma.book.update({
+        where: { id },
+        data: dto,
+      });
+      return book;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new NotFoundException('Book not found');
+        }
+      }
+      throw error;
+    }
   }
 
-  delete(id: string) {
-    return this.prisma.book.delete({
-      where: { id },
-    });
+  async delete(id: string) {
+    try {
+      const book = await this.prisma.book.delete({
+        where: { id },
+      });
+      return book;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new NotFoundException('Book not found');
+        }
+      }
+      throw error;
+    }
   }
 }
