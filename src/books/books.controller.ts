851f6264424d4fa32422a0a49758782867be7d59
Ulises <ulisesdejesusk9999@gmail.com>
2026-08-15 09:52:08 -0600

    feat(books): implement book CRUD operations
    - Add DTOs for book data validation.

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { BooksService } from './books.service';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('books')
 export class BooksController {
@@ -7,5 +17,23 @@ export class BooksController {
   @Get()
   findAll() {
     return this.booksService.findAll();
+  }
+  @Post()
+  create(@Body() dto: CreateBookDto) {
+    return this.booksService.create(dto);
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.booksService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateBookDto) {
+    return this.booksService.update(id, dto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.booksService.delete(id);
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,11 +1,39 @@
+import { PrismaService } from '@/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Injectable()
 export class BooksService {
+  constructor(private readonly prisma: PrismaService) {}
   findAll() {
-    return [
-      { id: 1, title: 'html basico', author: 'nicolas', available: true },
-      { id: 2, title: 'html basico 2', author: 'nicolas', available: true },
-    ];
+    return this.prisma.book.findMany();
+  }
+
+  create(dto: CreateBookDto) {
+    return this.prisma.book.create({
+      data: dto,
+    });
+  }
+
+  findOne(id: string) {
+    return this.prisma.book.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  update(id: string, dto: UpdateBookDto) {
+    return this.prisma.book.update({
+      where: { id },
+      data: dto,
+    });
+  }
+
+  delete(id: string) {
+    return this.prisma.book.delete({
+      where: { id },
+    });
   }
 }
