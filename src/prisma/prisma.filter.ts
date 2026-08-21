6822e480958fcfd53d4c54078559b6b9ce5618fe
Ulises <ulisesdejesusk9999@gmail.com>
2026-08-20 21:33:45 -0600

    feat(filters): add HttpExceptionFilter for error handling

--- src/prisma/prisma.filter.ts
+++ src/prisma/prisma.filter.ts
@@ -1,21 +1,22 @@
+import { sendError } from '@/common/utils/send-error';
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 
 const prismaErrors: Record<
   string,
   {
     status: number;
-    message: string;
+    message: string[];
   }
 > = {
   P2002: {
     status: 409,
-    message: 'A unique constraint was violated',
+    message: ['A unique constraint was violated'],
   },
   P2025: {
     status: 404,
-    message: 'Record not found',
+    message: ['Record not found'],
   },
 };
 
@@ -23,25 +24,21 @@ const prismaErrors: Record<
 export class PrismaFilter implements ExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
     const context = host.switchToHttp();
+
+    const request = context.getRequest<Request>();
     const response = context.getResponse<Response>();
 
     if (exception instanceof Prisma.PrismaClientKnownRequestError) {
       const error = prismaErrors[exception.code];
 
       if (!error) {
-        return this.sendError(response, 500, 'Internal server error');
+        return sendError(request, response, 500, ['Internal server error']);
       }
 
-      return this.sendError(response, error.status, error.message);
+      return sendError(request, response, error.status, error.message);
     }
 
-    return this.sendError(response, 500, 'Internal server error');
+    return sendError(request, response, 500, ['Internal server error']);
   }
 
-  private sendError(response: Response, status: number, message: string) {
-    return response.status(status).json({
-      message,
-      statusCode: status,
-    });
-  }
 }
